b6698eae345ea22dc99db957cb208b6cfdd22bd7
Dosalif <salifdo5036d@gmail.com>
2022-09-19 21:10:58 -0700

    Puts half

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+#include "stdio.h"
+
+
+
+/**
+ *
+ * * print_array - Print array upto n elements
+ *
+ * * @a: Array to be read from
+ *
+ * * @n: Number of elements to be read
+ *
+ * * Return: Always 0.
+ *
+ * */
+
+
+
+void print_array(int *a, int n)
+
+{
+
+		int i;
+
+
+
+			if (n > 0)
+
+					{
