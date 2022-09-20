2bc0f723b638952e3ef13bd6692ee7bb8d760fcf
Dosalif <salifdo5036d@gmail.com>
2022-09-19 21:21:37 -0700

    Number of elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,31 +1,35 @@
-#include "holberton.h"
-
+#include "0-main.h"
 #include "stdio.h"
 
-
-
 /**
- *
- * * print_array - Print array upto n elements
- *
- * * @a: Array to be read from
- *
- * * @n: Number of elements to be read
- *
- * * Return: Always 0.
- *
- * */
-
-
+* print_array - Print array upto n elements
+*
+* @a: Array to be read from
+*
+* @n: Number of elements to be read
+* Return: Always 0.
+*
+*/
 
 void print_array(int *a, int n)
 
 {
 
-		int i;
+	int i;
 
-
-
-			if (n > 0)
-
-					{
+	if (n > 0)
+	{
+		for (i = 0 ; n > i; i++)
+		{
+			if (i != n - 1)
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+				printf("%d", a[i]);
+			}
+		}
+	}
+	printf("\n");
+}
